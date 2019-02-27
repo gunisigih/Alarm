4486be237cbe230f07b559b984be3dbfb8b91b86
gunisigih <46692137+gunisigih@users.noreply.github.com>
2019-02-28 00:42:27 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAlarmMetni = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnAlarmKur = new System.Windows.Forms.Button();
+            this.btnAlarmDurdur = new System.Windows.Forms.Button();
+            this.tmrSaat = new System.Windows.Forms.Timer(this.components);
+            this.tmrZamanKontrolu = new System.Windows.Forms.Timer(this.components);
+            this.tmrAlarmIsleri = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.acToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cikisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Alarm.Properties.Resources.gong;
+            this.pictureBox1.Location = new System.Drawing.Point(449, 7);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 153);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tarih : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Saat : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 75);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Alarm Metni : ";
+            // 
+            // txtAlarmMetni
+            // 
+            this.txtAlarmMetni.Location = new System.Drawing.Point(133, 71);
+            this.txtAlarmMetni.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAlarmMetni.Multiline = true;
+            this.txtAlarmMetni.Name = "txtAlarmMetni";
+            this.txtAlarmMetni.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAlarmMetni.Size = new System.Drawing.Size(265, 88);
+            this.txtAlarmMetni.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(133, 7);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // btnAlarmKur
+            // 
+            this.btnAlarmKur.BackgroundImage = global::Alarm.Properties.Resources.alarmOn;
+            this.btnAlarmKur.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAlarmKur.Location = new System.Drawing.Point(447, 167);
+            this.btnAlarmKur.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAlarmKur.Name = "btnAlarmKur";
+            this.btnAlarmKur.Size = new System.Drawing.Size(55, 44);
+            this.btnAlarmKur.TabIndex = 7;
+            this.btnAlarmKur.UseVisualStyleBackColor = true;
+            this.btnAlarmKur.Click += new System.EventHandler(this.btnAlarmKur_Click);
+            // 
+            // btnAlarmDurdur
+            // 
+            this.btnAlarmDurdur.BackgroundImage = global::Alarm.Properties.Resources.alarmOff;
+            this.btnAlarmDurdur.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAlarmDurdur.Location = new System.Drawing.Point(509, 167);
+            this.btnAlarmDurdur.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAlarmDurdur.Name = "btnAlarmDurdur";
+            this.btnAlarmDurdur.Size = new System.Drawing.Size(55, 44);
+            this.btnAlarmDurdur.TabIndex = 8;
+            this.btnAlarmDurdur.UseVisualStyleBackColor = true;
+            this.btnAlarmDurdur.Click += new System.EventHandler(this.btnAlarmDurdur_Click);
+            // 
+            // tmrSaat
+            // 
+            this.tmrSaat.Enabled = true;
+            this.tmrSaat.Interval = 1000;
+            this.tmrSaat.Tick += new System.EventHandler(this.tmrSaat_Tick);
+            // 
+            // tmrZamanKontrolu
+            // 
+            this.tmrZamanKontrolu.Interval = 1000;
+            this.tmrZamanKontrolu.Tick += new System.EventHandler(this.tmrZamanKontrolu_Tick);
+            // 
+            // tmrAlarmIsleri
+            // 
+            this.tmrAlarmIsleri.Interval = 1000;
+            this.tmrAlarmIsleri.Tick += new System.EventHandler(this.tmrAlarmIsleri_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acToolStripMenuItem,
+            this.cikisToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(182, 84);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // acToolStripMenuItem
+            // 
+            this.acToolStripMenuItem.Name = "acToolStripMenuItem";
+            this.acToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.acToolStripMenuItem.Text = "Aç";
+            this.acToolStripMenuItem.Click += new System.EventHandler(this.acToolStripMenuItem_Click);
+            // 
+            // cikisToolStripMenuItem
+            // 
+            this.cikisToolStripMenuItem.Name = "cikisToolStripMenuItem";
+            this.cikisToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.cikisToolStripMenuItem.Text = "Cikis";
+            this.cikisToolStripMenuItem.Click += new System.EventHandler(this.cikisToolStripMenuItem_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(133, 39);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.maskedTextBox1.Mask = "00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(265, 22);
+            this.maskedTextBox1.TabIndex = 9;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 233);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.btnAlarmDurdur);
+            this.Controls.Add(this.btnAlarmKur);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtAlarmMetni);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAlarmMetni;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnAlarmKur;
+        private System.Windows.Forms.Button btnAlarmDurdur;
+        private System.Windows.Forms.Timer tmrSaat;
+        private System.Windows.Forms.Timer tmrZamanKontrolu;
+        private System.Windows.Forms.Timer tmrAlarmIsleri;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem acToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cikisToolStripMenuItem;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Alarm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string _alarmZamani;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = DateTime.Now.ToShortTimeString();
+        }
+
+        private void tmrSaat_Tick(object sender, EventArgs e)
+        {
+            //tmrSaat, her saniye sistem saatini alıp, form' un başlığında gösterecek
+            this.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void tmrZamanKontrolu_Tick(object sender, EventArgs e)
+        {//eğer alarmZamani değişkeni ile sistem saatinin değeri aynı ise, alarm vakti geldi demektir.
+            //tmrAlarmIsleri isimli timer' ı çalıştırdık, tmrZamanKontrolu isimli timer' ı durdurduk
+            if (_alarmZamani == DateTime.Now.ToShortTimeString())
+
+            {
+                tmrAlarmIsleri.Start();
+                tmrZamanKontrolu.Stop();
+            }
+        }
+
+        private void btnAlarmKur_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //btnAlarmKur butonu tıklandığında, alarmZamani isimli değişkene, txtSaat' in text' ini değer olarak atadık
+                _alarmZamani = maskedTextBox1.Text;
+                tmrZamanKontrolu.Start();
+                MessageBox.Show(string.Format(@"Alarm {0} olarak kuruldu", _alarmZamani ));
+                this.Opacity = 0;
+                this.ShowInTaskbar = false;
+            }
+            catch
+            {
+                MessageBox.Show(@"Uygun bir saat giriniz");
+            }
+        }
+
+        private void tmrAlarmIsleri_Tick(object sender, EventArgs e)
+        {
+            //Windows Ding.wav dosyasını bin\Debug klasörüne ekledik
+            SoundPlayer sp = new SoundPlayer();
+            //sp.SoundLocation = "C:\\Windows Ding.wav";
+
+            sp.SoundLocation = string.Format(@"{0}\Windows Ding.wav", Application.StartupPath);
+            sp.Play();
+
+            //formu görünür yaptık
+            this.Opacity = 1;
+            //görev çubuğunda gösterdik
+            this.ShowInTaskbar = true;
+            //formu öne taşıdık
+            this.BringToFront();
+
+            //formu her zaman en önde tutar.
+            this.TopMost = true;
+
+
+            //aşağıdaki işlemler 400 kere tekrarlanacak
+            for (int i = 0; i < 400; i++)
+            {
+                pictureBox1.Left += 2;
+                pictureBox1.Left -= 2;
+                pictureBox1.Top += 2;
+                pictureBox1.Top -= 2;
+            }
+
+            if (txtAlarmMetni.BackColor == Color.White)
+            {
+                txtAlarmMetni.BackColor = Color.Red;
+                txtAlarmMetni.ForeColor = Color.White;
+            }
+            else
+            {
+                txtAlarmMetni.BackColor = Color.White;
+                txtAlarmMetni.ForeColor = Color.Red;
+            }
+        }
+
+        private void btnAlarmDurdur_Click(object sender, EventArgs e)
+        {
+            tmrAlarmIsleri.Stop();
+        }
+
+        private void acToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
+            this.ShowInTaskbar = true;
+            //Formu ekranın ortasına getirir.
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void cikisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //MsjBox' ta YES NO butonları çıkar, eğer YES' e basılırsa DialogResult.YES demektir bu.
+            if (MessageBox.Show(@"Çıkmak istediğinizden emin misiniz?", @"ÇIKIŞ", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //
+        }
+    }
+}
